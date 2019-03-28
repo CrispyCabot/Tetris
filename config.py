5b01c790ebf38d7d0ad168b0e5d7ffa625f90ad6
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2019-03-28 12:39:37 -0400

    default values

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-width = 420 #multiple of size
-height = 595 #multiple of size
+# defaults: width - 400 height - 600 size - 20
+width = 400 #multiple of size
+height = 600 #multiple of size
 
-size = 35
+size = 20
 
 columns = width/size
